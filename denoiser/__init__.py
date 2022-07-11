3eef03e184cada781103fb87e5709c74014e4186
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2022-07-11 16:58:11 +0200

    [refactor] Remove package level import

--- denoiser/__init__.py
+++
@@ -1,9 +0,0 @@
-from .denoise import hybrid_pca, local_lowrank_denoise, mp_pca, nordic, raw_svt
-
-__all__ = [
-    "hybrid_pca",
-    "local_lowrank_denoise",
-    "mp_pca",
-    "nordic",
-    "raw_svt",
-]
