96be7d07f972aafd4a7e704394b58828caffd307
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2022-07-11 14:44:00 +0200

    export only the functional API

---
+++ denoiser/__init__.py
@@ -0,0 +1,9 @@
+from .denoise import hybrid_pca, local_lowrank_denoise, mp_pca, nordic, raw_svt
+
+__all__ = [
+    "hybrid_pca",
+    "local_lowrank_denoise",
+    "mp_pca",
+    "nordic",
+    "raw_svt",
+]
