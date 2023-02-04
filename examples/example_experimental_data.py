b701a1ce85aba19b94a3491d3bad19252a403601
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2023-02-04 18:45:48 +0100

    feat(docs): start experimental example.

--- examples/example_experimental_data.py
+++ examples/example_experimental_data.py
@@ -7,5 +7,16 @@ This is a example script to test various denoising methods on real-word fMRI dat
 Source data should a sequence of 2D or 3D data, the temporal dimension being the last one.
 
 The available denoising methods are "nordic", "mp-pca", "hybrid-pca", "opt-fro", "opt-nuc" and "opt-op".
-
 """
+
+from denoiser.simulation.phantom import mr_shepp_logan_t2_star, g_factor_map
+from denoiser.simulation.activations import add_frames
+from denoiser.simulation.noise import add_temporal_gaussian_noise
+
+# %%
+# Setup the parameters for the simulation and noise
+
+SHAPE = (64, 64, 64)
+N_FRAMES = 200
+
+NOISE_LEVEL = 2
