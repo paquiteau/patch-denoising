6f53209b1573266057f4bacc13a462edb8147c40
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2023-02-06 14:27:41 +0100

    refactor(cli): delete base utility function.

--- src/patch_denoise/bindings/utils.py
+++ src/patch_denoise/bindings/utils.py
@@ -1,6 +1,8 @@
 """Common utilities for bindings."""
 from dataclasses import dataclass
 
+import numpy as np
+
 from patch_denoise.denoise import (
     hybrid_pca,
     mp_pca,
@@ -9,6 +11,12 @@ from patch_denoise.denoise import (
     raw_svt,
     adaptive_thresholding,
 )
+
+NIBABEL_AVAILABLE = True
+try:
+    import nibabel as nib
+except ImportError:
+    NIBABEL_AVAILABLE = False
 
 DENOISER_MAP = {
     None: None,
@@ -88,3 +96,33 @@ class DenoiseParameters:
         if self.mask_threshold:
             ret_str += f"_{self.mask_threshold}"
         return ret_str
+
+
+def load_complex_nifti(mag_file, phase_file, filename=None):  # pragma: no cover
+    """Load two nifti image (magnitude and phase) to create a complex valued array.
+
+    Optionally, the result can be save as a .npy file
+
+    Parameters
+    ----------
+    mag_file: str
+        The source magnitude file
+    phase_file: str
+        The source phase file
+    filename: str, default None
+        The output filename
+    """
+    if not NIBABEL_AVAILABLE:
+        raise RuntimeError(
+            "nibabel is not available, please install it to load experimental data"
+        )
+
+    mag = nib.load(mag_file).get_fdata()
+    phase = nib.load(phase_file).get_fdata()
+    print(np.min(phase), np.max(phase))
+    print(np.min(mag), np.max(mag))
+    img = mag * np.exp(1j * phase)
+
+    if filename is not None:
+        np.save(filename, img)
+    return img
