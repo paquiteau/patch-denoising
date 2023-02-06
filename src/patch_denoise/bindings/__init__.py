f1734132d7a34197132aa80ee0bae9eaf0fdd17f
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2023-02-06 09:38:12 +0100

    add module description for bindings.

---
+++ src/patch_denoise/bindings/__init__.py
@@ -0,0 +1,8 @@
+"""Bindings for different external packages.
+
+3 Bindings are available:
+
+* A `Nipype`<https://github.com/nipype/nipype/>_ binding
+* A `Modopt`<https://cea-cosmic.github.io/ModOpt/index.html>_ binding
+* A Command line interface, available as ``patch-denoise`` after installation.
+"""
