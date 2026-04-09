18d39341076f27d14c5c6ece73dcf7efde33b083
Remi Gau <remi_gau@hotmail.com>
2026-04-09 16:44:14 +0200

    bids app!!!!

--- src/patch_denoise/bindings/cli.py
+++ src/patch_denoise/bindings/cli.py
@@ -2,12 +2,16 @@
 """Cli interface."""
 
 import argparse
+import json
 import logging
 import re
 from functools import partial
 from pathlib import Path
 
 import numpy as np
+from nilearn.image import load_img
+from nilearn.interfaces.bids import get_bids_files, parse_bids_filename
+from nilearn.interfaces.bids.utils import bids_entities, create_bids_filename
 from nilearn.maskers import NiftiMasker
 
 from patch_denoise import __version__
@@ -100,8 +104,8 @@ def _get_parser():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
+
     IsFile = partial(_is_file, parser=parser)
-    TuplePositiveInt = partial(_tuple_positive_int, is_parser=True)
 
     parser.add_argument(
         "input_file",
@@ -115,6 +119,78 @@ def _get_parser():
         type=Path,
         help=("Output (denoised) file.\nDefault is D<input_file_name>."),
     )
+    return _extend_parser(parser)
+
+
+def _bids_parser():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument(
+        "bids_dir",
+        action="store",
+        type=Path,
+        help="The directory with the input dataset formatted according to the "
+        "BIDS standard.",
+    )
+    parser.add_argument(
+        "output_dir",
+        action="store",
+        type=Path,
+        help="The directory where the output files should be stored.",
+    )
+    parser.add_argument(
+        "analysis_level",
+        help="Level of the analysis that will be performed. Only participant"
+        "level is available.",
+        choices=["participant"],
+    )
+    parser.add_argument(
+        "--participant-label",
+        "--participant_label",
+        help="The label(s) of the participant(s) to analyze. The "
+        "label corresponds to sub-<participant-label> from the BIDS spec (so "
+        "it does not include 'sub-'). If this parameter is not provided all "
+        "subjects will be analyzed. Multiple participants can be specified "
+        "with a space separated list.",
+        nargs="+",
+    )
+    parser.add_argument(
+        "--session-label",
+        "--session_label",
+        help="The label(s) of the session(s) to analyze. The "
+        "label corresponds to ses-<participant-label> from the BIDS spec (so "
+        "it does not include 'ses-'). If this parameter is not provided all "
+        "sessions will be analyzed. Multiple sessions can be specified "
+        "with a space separated list.",
+        nargs="+",
+    )
+    parser.add_argument(
+        "--task-label",
+        "--task_label",
+        help="The label(s) of the task(s) to analyze. The "
+        "label corresponds to task-<participant-label> from the BIDS spec (so "
+        "it does not include 'task-'). If this parameter is not provided all "
+        "tasks will be analyzed. Multiple tasks can be specified "
+        "with a space separated list.",
+        nargs="+",
+    )
+    parser.add_argument(
+        "--bids-filter-file",
+        type=Path,
+        help="A JSON file describing custom BIDS input filters using PyBIDS. "
+        "We use the same format as described in fMRIPrep documentation: "
+        "https://fmriprep.org/en/latest/faq.html#"
+        "how-do-i-select-only-certain-files-to-be-input-to-fmriprep "
+        "\nHowever, the query filed should always be 'bold'",
+    )
+    return _extend_parser(parser, bids_app=True)
+
+
+def _extend_parser(parser, bids_app=False):
+    IsFile = partial(_is_file, parser=parser)
+    TuplePositiveInt = partial(_tuple_positive_int, is_parser=True)
 
     parser.add_argument("--version", action="version", version=__version__)
 
@@ -218,56 +294,58 @@ def _get_parser():
         action=ToDict,
     )
 
-    data_group = parser.add_argument_group("Additional input data")
-    data_group.add_argument(
-        "--mask",
-        metavar="FILE|auto",
-        default=None,
-        help=("mask file, if auto, it would be determined from the average image."),
-    )
-    data_group.add_argument(
-        "--noise-map",
-        metavar="FILE",
-        default=None,
-        type=IsFile,
-        help="noise map estimation file",
-    )
-    data_group.add_argument(
-        "--input-phase",
-        metavar="FILE",
-        default=None,
-        type=IsFile,
-        help=(
-            "Phase of the input data. This MUST be in radians. "
-            "No rescaling will be applied."
-        ),
-    )
-    data_group.add_argument(
-        "--noise-map-phase",
-        metavar="FILE",
-        default=None,
-        type=IsFile,
-        help=(
-            "Phase component of the noise map estimation file. "
-            "This MUST be in radians. No rescaling will be applied."
-        ),
-    )
+    if not bids_app:
+        data_group = parser.add_argument_group("Additional input data")
+        data_group.add_argument(
+            "--mask",
+            metavar="FILE|auto",
+            default=None,
+            help=("mask file, if auto, it would be determined from the average image."),
+        )
+        data_group.add_argument(
+            "--noise-map",
+            metavar="FILE",
+            default=None,
+            type=IsFile,
+            help="noise map estimation file",
+        )
+        data_group.add_argument(
+            "--input-phase",
+            metavar="FILE",
+            default=None,
+            type=IsFile,
+            help=(
+                "Phase of the input data. This MUST be in radians. "
+                "No rescaling will be applied."
+            ),
+        )
+        data_group.add_argument(
+            "--noise-map-phase",
+            metavar="FILE",
+            default=None,
+            type=IsFile,
+            help=(
+                "Phase component of the noise map estimation file. "
+                "This MUST be in radians. No rescaling will be applied."
+            ),
+        )
 
     misc_group = parser.add_argument_group("Miscellaneous options")
-    misc_group.add_argument(
-        "--output-noise-map",
-        metavar="FILE",
-        default=None,
-        type=Path,
-        help="Output name for calculated noise map",
-    )
-    misc_group.add_argument(
-        "--nan-to-num",
-        metavar="VALUE",
-        default=None,
-        type=float,
-        help="Replace NaN by the provided value.",
-    )
+    if not bids_app:
+        misc_group.add_argument(
+            "--output-noise-map",
+            metavar="FILE",
+            default=None,
+            type=Path,
+            help="Output name for calculated noise map",
+        )
+        misc_group.add_argument(
+            "--nan-to-num",
+            metavar="VALUE",
+            default=None,
+            type=float,
+            help="Replace NaN by the provided value.",
+        )
     misc_group.add_argument(
         "-v",
         "--verbose",
@@ -282,10 +360,6 @@ def parse_args():
     """Parse input arguments."""
     parser = _get_parser()
     args = parser.parse_args()
-
-    # default value for output.
-    if args.output_file is None:
-        args.output_file = args.input_file.with_stem("D" + args.input_file.stem)
 
     if not args.extra:
         args.extra = {}
@@ -381,17 +455,17 @@ def main():
     masker.mask_img_.to_filename(mask_filename)
 
     if args.gpu:
-        if GPU_AVAILABLE:
-            logging.info("Using GPU for computation.")
-            denoised_data, _, noise_std_map = main_gpu(
-                args, input_data, mask, noise_map, **kwargs
-            )
-        else:
+        if not GPU_AVAILABLE:
             raise RuntimeError(
                 "GPU support is not available. Please ensure that the "
                 "patch_denoise.gpu module is installed and that you have"
                 "a compatible GPU."
             )
+        logging.info("Using GPU for computation.")
+        denoised_data, _, noise_std_map = main_gpu(
+            args, input_data, mask, noise_map, **kwargs
+        )
+
     else:
         if args.method in [
             "nordic",
@@ -418,6 +492,166 @@ def main():
 
     save_array(denoised_data, affine, args.output_file)
     save_array(noise_std_map, affine, args.output_noise_map)
+
+
+def bids_app():
+    """Run CLI for bids app."""
+    parser = _bids_parser()
+    args = parser.parse_args()
+
+    if not args.extra:
+        args.extra = {}
+    kwargs = args.extra
+
+    levels = [logging.WARNING, logging.INFO, logging.DEBUG]
+    level = levels[min(args.verbose, len(levels) - 1)]  # cap to last level index
+    logging.basicConfig(level=level)
+
+    if args.participant_label:
+        all_subjects = args.participant_label
+    else:
+        all_subjects = [
+            x.name.strip("sub-")
+            for x in Path(args.bids_dir).iterdir()
+            if "sub-" in x.name
+        ]
+
+    filters = []
+    if args.task_label:
+        filters.append(("task", args.task_label[0]))
+    if args.session_label:
+        filters.append(("ses", args.session_label[0]))
+
+    # Parse configuration string instead of defining each parameter separately
+    if args.conf is not None:
+        d_par = DenoiseParameters.from_str(args.conf)
+        args.method = d_par.method
+        args.patch_shape = d_par.patch_shape
+        args.patch_overlap = d_par.patch_overlap
+        args.recombination = d_par.recombination
+        args.mask_threshold = d_par.mask_threshold
+
+    logging.info(f"Current Setup {args}")
+
+    noise_map = None
+
+    if args.gpu and not GPU_AVAILABLE:
+        raise RuntimeError(
+            "GPU support is not available. Please ensure that the "
+            "patch_denoise.gpu module is installed and that you have"
+            "a compatible GPU."
+        )
+    else:
+        if args.method in [
+            "nordic",
+            "hybrid-pca",
+            "adaptive-qut",
+            "optimal-fro-noise",
+        ]:
+            if noise_map is None:
+                raise RuntimeError("A noise map must be specified for this method.")
+            kwargs["noise_std"] = noise_map
+        denoise_func = DENOISER_MAP.get(args.method, None)
+        if denoise_func is None:
+            raise ValueError(f"Method {args.method} is not supported.")
+
+    output_dir = Path(args.output_dir)
+    output_dir.mkdir(exist_ok=True, parents=True)
+
+    ds_json = output_dir / "dataset_description.json"
+    GeneratedBy = {
+        "Name": "nilearn",
+        "Version": __version__,
+        "Description": ("A dataset denoised with patch-denoise."),
+        "CodeURL": "",
+    }
+    if ds_json.exists():
+        with ds_json.open() as f_obj:
+            dataset_description = json.load(f_obj)
+        if dataset_description.get("GeneratedBy"):
+            dataset_description["GeneratedBy"].append(GeneratedBy)
+    else:
+        dataset_description = {
+            "BIDSVersion": "1.9.0",
+            "DatasetType": "derivative",
+            "GeneratedBy": [GeneratedBy],
+        }
+    with ds_json.open("w") as f_obj:
+        json.dump(dataset_description, f_obj, indent=4, sort_keys=True)
+
+    entities_to_include = bids_entities()["raw"] + bids_entities()["derivatives"]
+
+    for sub_label in all_subjects:
+        files = get_bids_files(
+            args.bids_dir,
+            file_tag="bold",
+            modality_folder="func",
+            file_type="nii.*",
+            sub_label=sub_label,
+            filters=filters,
+        )
+
+        for f in files:
+            parsed_f = parse_bids_filename(f)
+
+            output_dir_for_subject = output_dir / f"sub-{parsed_f['entities']['sub']}"
+            if "ses" in parsed_f["entities"]:
+                output_dir_for_subject = (
+                    output_dir_for_subject / f"ses-{parsed_f['entities']['ses']}"
+                )
+
+            parsed_f["entities"]["desc"] = "denoised"
+            output_filename = output_dir_for_subject / create_bids_filename(
+                parsed_f,
+                entities_to_include=entities_to_include,
+            )
+            parsed_f["suffix"] = "mask"
+            output_mask_filename = output_dir_for_subject / create_bids_filename(
+                parsed_f,
+                entities_to_include=entities_to_include,
+            )
+            parsed_f["suffix"] = "std"
+            output_std_filename = output_dir_for_subject / create_bids_filename(
+                parsed_f,
+                entities_to_include=entities_to_include,
+            )
+
+            output_dir_for_subject.mkdir(exist_ok=True, parents=True)
+
+            masker = NiftiMasker(verbose=args.verbose, mask_strategy="epi")
+            masker.fit(f)
+            mask = masker.mask_img_.get_fdata().astype(bool)
+            affine = masker.mask_img_.affine
+
+            report = masker.generate_report()
+            report.save_as_html(output_mask_filename.with_suffix(".html"))
+
+            masker.mask_img_.to_filename(output_mask_filename)
+
+            input_data = load_img(f).get_fdata()
+
+            if args.gpu:
+                denoised_data, _, noise_std_map = main_gpu(
+                    args,
+                    input_data,
+                    mask,
+                    # noise_map,
+                    # **kwargs
+                )
+            else:
+                denoised_data, _, noise_std_map, _ = denoise_func(
+                    input_data,
+                    patch_shape=args.patch_shape,
+                    patch_overlap=args.patch_overlap,
+                    mask=mask,
+                    mask_threshold=args.mask_threshold,
+                    recombination=args.recombination,
+                    # **kwargs,
+                )
+
+            print(noise_std_map.shape)
+            save_array(denoised_data, affine, output_filename)
+            save_array(noise_std_map, affine, output_std_filename)
 
 
 if __name__ == "__main__":
